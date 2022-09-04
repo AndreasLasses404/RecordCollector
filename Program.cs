78a2d7cdb0fd3a0edd68a3fa923d3dedb12f61db
Andreas Lasses <andreas.lasses404@outlook.com>
2022-09-04 20:58:50 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSwaggerGen(options =>
     options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
     {
         Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
-        In = ParameterLocation.Header,
+        In = ParameterLocation.Header, 
         Name = "Authorization",
         Type = SecuritySchemeType.ApiKey
     });
